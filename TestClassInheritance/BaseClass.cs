4e91f845d891fe280a404170fb96a80f41d31200
mikki11 <miroslav.mdv@gmail.com>
2018-10-07 08:53:53 +0200

    Add TestClassInheritance Console Project

---
+++ TestClassInheritance/BaseClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassInheritance
+{
+    public class BaseClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("BaseClass: Method1 ");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("BaseClass: Method2 ");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+
+    }
+}
